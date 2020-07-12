8914e22d5ca806e78b49df71819f23c2738c721f
tthandb <tthandb@hotmail.com>
2020-07-12 18:02:16 +0700

    init project

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
 
+const data = [
+  {
+    id: 1,
+    uri: require("./assets/album/1.jpg"),
+  },
+  {
+    id: 2,
+    uri: require("./assets/album/2.jpg"),
+  },
+  {
+    id: 3,
+    uri: require("./assets/album/3.jpg"),
+  },
+  {
+    id: 4,
+    uri: require("./assets/album/4.jpg"),
+  },
+  {
+    id: 5,
+    uri: require("./assets/album/5.jpg"),
+  },
+  {
+    id: 6,
+    uri: require("./assets/album/6.jpg"),
+  },
+  {
+    id: 7,
+    uri: require("./assets/album/7.jpg"),
+  },
+  {
+    id: 8,
+    uri: require("./assets/album/8.jpg"),
+  },
+];
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          <Image source={item.uri} />;
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
